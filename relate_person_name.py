12185b9a56efbdaf0d42aa359e597f76ffbc2892
Kenta Aikawa <kalupas226@gmail.com>
2020-09-21 09:48:55 +0900

    Update relate_person_name.py

--- relate_person_name.py
+++ relate_person_name.py
@@ -3,7 +3,7 @@ import pymysql.cursors
 conn = pymysql.connect(host='localhost',
                        db='Research_System',
                        user='root',
-                       passwd='tyogyakuten226',
+                       passwd='',
                        charset='utf8',
                        cursorclass=pymysql.cursors.DictCursor)
 
